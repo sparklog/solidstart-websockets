4af631b5968fe230cfebb2c2618348a53e4fe584
tungliang <tungliang.hannus@gmail.com>
2024-11-23 21:01:19 +0800

    update: edit for implement wss

--- src/routes/(root)/index.tsx
+++ src/routes/(root)/index.tsx
@@ -6,7 +6,13 @@ export default function Sockets() {
   return (
     <div class="grid place-items-center pt-10">
       <h1 class="text-md text-neutral-500 font-bold pb-2">SolidChat</h1>
-      <WebSocketClient initialSocketUrl="ws://localhost:3000/ws" />
+      <WebSocketClient
+        initialSocketUrl={
+          import.meta.env.PROD
+            ? `wss://${import.meta.env.DENO_DEPLOYMENT_ID}.deno.dev/ws`
+            : "ws://localhost:3000/ws"
+        }
+      />
     </div>
   );
 }
