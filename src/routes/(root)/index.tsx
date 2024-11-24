01098d3319a5dd18257a9cade09aebfc988f0d65
tungliang <tungliang.hannus@gmail.com>
2024-11-24 15:26:41 +0800

    update: update wss

--- src/routes/(root)/index.tsx
+++ src/routes/(root)/index.tsx
@@ -4,10 +4,11 @@ const WebSocketClient = clientOnly(() => import("../../components/ws-client"));
 
 export default function Sockets() {
   // 在服务端计算 wsUrl
-  const wsUrl = import.meta.env.PROD
-    ? `wss://${import.meta.env.DENO_DEPLOYMENT_ID}.deno.dev/ws`
-    : "ws://localhost:3000/ws";
-  ``;
+  const wsUrl = "wss://hannus-solidstart-61.deno.dev/ws";
+  // import.meta.env.PROD
+  //   ? `wss://${import.meta.env.DENO_DEPLOYMENT_ID}.deno.dev/ws`
+  //   : "ws://localhost:3000/ws";
+  // ``;
   return (
     <div class="grid place-items-center pt-10">
       <h1 class="text-md text-neutral-500 font-bold pb-2">SolidChat</h1>
